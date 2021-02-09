9c4d2c0cede5e587688518ec99f6e2f1f2689159
Jakub Supa <jakub.supa@icloud.com>
2021-02-09 19:43:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,22 +2,22 @@ var http = require('http')
 	url = require('url');
 
 var server = http.createServer(function(req,res){
-	var ParsedUrl = url.parse(req.url,true),
-		Path = ParsedUrl.pathname
-		TrimmedPath = Path.replace(/^\/+|\/+$/g,'');
-	
-	var checkHello = TrimmedPath == 'hello' ? Write('hello world') : Write('404');
+	var ParsedUrl = url.parse(req.url, true),
+		Path = ParsedUrl.pathname;
+	TrimmedPath = Path.replace(/^\/+|\/+$/g, "");
 
-	function Write(message){
-			var Payload = {message},
-				PayloadString = JSON.stringify(Payload);
+	var checkHello = TrimmedPath == "hello" ? Write("hello world") : Write("404");
 
-			res.setHeader('Content-Type', 'application/json');
-			res.writeHead(200);
-			res.end(PayloadString);
+	function Write(message) {
+		var Payload = { message },
+			PayloadString = JSON.stringify(Payload);
+
+		res.setHeader("Content-Type", "application/json");
+		res.writeHead(200);
+		res.end(PayloadString);
 	}
 });
 
-server.listen(1337, function(){
-	console.log('Server listening on port http://localhost:1337');
+server.listen(1337, function () {
+	console.log("Server listening on port http://localhost:1337");
 });
